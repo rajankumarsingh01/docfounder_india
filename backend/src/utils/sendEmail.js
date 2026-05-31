113af79348ae83e98652719f6e164415fdeae12b
Rajan <cpsrajan2002@gmail.com>
2026-05-31 20:36:36 +0530

    changes port

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -5,7 +5,7 @@ const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: env.EMAIL_HOST,
     port: env.EMAIL_PORT,
-    secure: false,
+    secure: true, // true for 465, false for other ports
     auth: {
       user: env.EMAIL_USER,
       pass: env.EMAIL_PASS,
