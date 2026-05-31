7aaf11e5663f5e3e3cfc259daa9831e71f83be6b
Rajan <cpsrajan2002@gmail.com>
2026-05-31 21:30:11 +0530

    2

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -4,11 +4,11 @@ const env = require("../config/env");
 const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: env.EMAIL_HOST,
-    port: env.EMAIL_PORT,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: env.EMAIL_USER,
-      pass: env.EMAIL_PASS,
+  port: env.EMAIL_PORT,
+  secure: env.EMAIL_PORT == 465,
+  auth: {
+    user: env.EMAIL_USER,
+    pass: env.EMAIL_PASS,
     },
   });
 
